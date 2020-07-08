c438ed2975cfc00786646e5bef71ab7f6c6b8714
atulrana97 <atulrana97@gmail.com>
2020-07-08 19:40:21 +0530

    UserLogin Update

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -13,37 +14,176 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final GoogleSignIn googleSignIn = GoogleSignIn();
-  final FirebaseAuth fireBaseAuth= FirebaseAuth.instance;
+  final FirebaseAuth fireBaseAuth = FirebaseAuth.instance;
+  TextEditingController _email = TextEditingController();
+  TextEditingController _password = TextEditingController();
+  TextEditingController _name = TextEditingController();
   SharedPreferences preferences;
+  final _formKey = GlobalKey<FormState>();
   bool loading;
-  bool isLogedin = false  ;
+  bool isLogedin = false;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     isSignedIn();
+  }
+
+  void isSignedIn() async {
+    setState(() {
+      loading = true;
+    });
+    preferences = await SharedPreferences.getInstance();
+    isLogedin = await googleSignIn.isSignedIn();
+    if (isLogedin) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    }
+    setState(() {
+      loading = false;
+    });
+  }
+
+  Future handleSignIn() async {
+    preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      loading = true;
+    });
+
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+    final AuthResult authResult =
+        await fireBaseAuth.signInWithCredential(credential);
+    final FirebaseUser firebaseUser = authResult.user;
+
+    if (firebaseUser != null) {
+      final QuerySnapshot result = await Firestore.instance
+          .collection("users")
+          .where("id", isEqualTo: firebaseUser.uid)
+          .getDocuments();
+      final List<DocumentSnapshot> documents = result.documents;
+      if (documents.length == 0) {
+        //insert the user to our collection
+        Firestore.instance
+            .collection("users")
+            .document(firebaseUser.uid)
+            .setData({
+          "id": firebaseUser.uid,
+          "username": firebaseUser.displayName,
+          "profilePicture": firebaseUser.photoUrl
+        });
+        await preferences.setString("id", firebaseUser.uid);
+        await preferences.setString("username", firebaseUser.displayName);
+        await preferences.setString("photourl", firebaseUser.displayName);
+      } else {
+        await preferences.setString("id", documents[0]['id']);
+        await preferences.setString("username", documents[0]['username']);
+        await preferences.setString("photourl", documents[0]['photourl']);
       }
 
-    void isSignedIn() async{
-          setState((){
-            loading = true;
-          });
-          preferences = await SharedPreferences.getInstance();
-          isLogedin = await googleSignIn.isSignedIn();
-          if(isLogedin){
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> HomePage()));
-          }
-          setState(() {
-            loading = false;
-          });
+      FlutterToast.showToast(msg: "Login was Successful");
+      setState(() {
+        loading = false;
+      });
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    } else {
+      FlutterToast.showToast(msg: "Login Failed :(");
     }
-    
-    @override
-    Widget build(BuildContext context) {
-      return Container(
-        
-      );
-    }
-    
-      
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        Image.asset(
+          'images/backgroundforLogin.jpg',
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+        Container(
+            alignment: Alignment.topCenter,
+            child: Image.asset('images/BakkiicookiiLogo.png',
+                width: 200, height: 200)),
+        Container(
+            alignment: Alignment.center,
+            child: Center(
+                child: Form(
+                    key: _formKey,
+                    child: ListView(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.grey.withOpacity(0.2),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: ListTile(
+                                title: TextFormField(
+                                  controller: _email,
+                                  decoration: InputDecoration(
+                                      hintText: "Email",
+                                      icon: Icon(Icons.alternate_email),
+                                      border: InputBorder.none),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      Pattern pattern =
+                                          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                                      RegExp regex = new RegExp(pattern);
+                                      if (!regex.hasMatch(value))
+                                        return 'Please make sure your email address is valid';
+                                      else
+                                        return null;
+                                    }
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )))),
+        Visibility(
+            visible: loading ?? true,
+            child: Center(
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.white.withOpacity(0.7),
+                child: CircularProgressIndicator(
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(Colors.red.shade900),
+                ),
+              ),
+            )),
+      ]),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: const EdgeInsets.only(
+              left: 12.0, right: 12.0, top: 8.0, bottom: 8.0),
+          child: FlatButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              onPressed: () {
+                handleSignIn();
+              },
+              color: Colors.red.shade900,
+              child: Text("Sign in/ Sign Up With Google",
+                  style: TextStyle(color: Colors.white))),
+        ),
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import './Pages/LoginPage.dart';
 
-
 //File Imports(Own Files)
 
-
-void main(){
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Login(),
-    )
-  );
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Login(),
+  ));
 }
-
-
